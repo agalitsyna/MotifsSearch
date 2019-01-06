1ffab415477eab255c832afdb2644da299c92ab7
Aleksandra Galitsyna <agalitzina@gmail.com>
2019-01-07 02:56:11 +0300

    SARUS git clone added

--- wd/00_download_autosome_software.sh
+++ wd/00_download_autosome_software.sh
@@ -1,3 +1,5 @@
 wget http://opera.autosome.ru/downloads/ape.jar
 wget http://autosome.ru/ChIPMunk/chipmunk.jar
-wget http://autosome.ru/ChIPMunk/sarus.jar
+#wget http://autosome.ru/ChIPMunk/sarus.jar
+git clone git@github.com:VorontsovIE/sarus.git 
+cp sarus/sarus-latest.jar sarus.jar
